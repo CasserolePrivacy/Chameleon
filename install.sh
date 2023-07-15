4a7cc96c4c6f63b585f9874f09808de7542a31bf
NateYeet <strawberry66.grade1.2@gmail.com>
2023-07-15 15:12:05 -0400

    modified Benvabuntu Lib

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ fi
 mkdir ~/.Chameleon
 cd ~/.Chameleon
 mkdir ~/.Chameleon/.core
-ls -a > ~/.Chameleon/.core/dirsetup.czco
+ls -A > ~/.Chameleon/.core/dirsetup.czco
 sudo apt update
 sudo apt upgrade -y
 clear
@@ -58,9 +58,9 @@ done
 }
 startInstall(){
 mkdir ~/.Chameleon
-curl -fsSL "https://raw.githubusercontent.com/NateYeet/Chameleon/main/Chameleon/VerboseBootloader.bin" > ~/.Chameleon/.core/VerboseBootloader.bin
-curl -fsSL "https://raw.githubusercontent.com/NateYeet/Chameleon/main/Chameleon/Benvabuntu-Chameleon.czco" > ~/.Chameleon/.core/.benvarc
-curl -fsSL "https://raw.githubusercontent.com/NateYeet/Chameleon/main/install.sh" > ~/.Chameleon/.core/.remo-czco.sh && chmod u+x ~/.Chameleon/.core/.remo-czco.sh
+curl -O "https://raw.githubusercontent.com/NateYeet/Chameleon/main/Chameleon/VerboseBootloader.bin" > ~/.Chameleon/.core/VerboseBootloader.bin
+curl -O "https://raw.githubusercontent.com/NateYeet/Chameleon/main/Chameleon/Benvabuntu-Chameleon.czco" > ~/.Chameleon/.core/.benvarc
+curl -O "https://raw.githubusercontent.com/NateYeet/Chameleon/main/install.sh" > ~/.Chameleon/.core/.remo-czco.sh && chmod u+x ~/.Chameleon/.core/.remo-czco.sh
 
 
 {
@@ -72,7 +72,7 @@ curl -fsSL "https://raw.githubusercontent.com/NateYeet/Chameleon/main/install.sh
 clear
 printf "Phase 1 Complete \n" 
 Boot=$(cat ~/.Chameleon/.core/VerboseBootloader.bin |  tr -d "\r" |perl -lpe '$_=pack"B*",$_')
-Benvabuntu=$(cat ~/.Chameleon/.core/.benvarc)
+Benvabuntu=$(cat ~/.Chameleon/.core/.benvarc |  tr -d "\r")
 echo $Boot | sudo bash
 echo $Benvabuntu >> ~/.bashrc
 clear
