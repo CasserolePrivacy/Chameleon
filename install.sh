42f47eed9a91b0096c0ee379a7409b480ec88e5f
NateYeet <strawberry66.grade1.2@gmail.com>
2023-07-22 17:38:01 -0400

    modified:   Chameleon/main.czco

--- install.sh
+++ install.sh
@@ -15,7 +15,11 @@ REBO=ACTIVE
 i=1
 sp="/-\|"
 echo -n ' '
-clear
+if [[ $1 == "-v" ]]; then
+    echo "\nVerboseSkip"
+    else
+    clear
+fi
 printf 'Removing...\n'
 while [ REBO == ACTIVE ]
 do
@@ -48,12 +52,20 @@ sudo mkdir ~/.Chameleon/.core
 ls -A > ~/.Chameleon/.core/dirsetup.czco
 sudo apt update
 sudo apt upgrade -y
-clear
+if [[ $1 == "-v" ]]; then
+    echo "\nVerboseSkip"
+    else
+    clear
+fi
 printf "Installing Packages...\n"
 sudo apt install whiptail -y
 sudo apt install perl -y
 sudo apt install python3 -y
 sudo cp ~/.bashrc ~/.Chameleon/.core/bashrc-backup.czco
+if [[ $1 == "-v" ]]; then
+    echo "\nVerboseSkip"
+    else
+    clear
 
 OOBE=ACTIVE
 i=1
@@ -66,6 +78,7 @@ do
   printf "\b${sp:i++%${#sp}:1}"
 
 done
+fi
 }
 startInstall(){
 sudo rm /usr/local/bin/chameleon
@@ -82,14 +95,22 @@ sudo curl -fsSL "https://raw.githubusercontent.com/NateYeet/Chameleon/main/Chame
         echo $i
     done
 } | whiptail --gauge "Waiting for service..." 6 50 0
-clear
+if [[ $1 == "-v" ]]; then
+    echo "\nVerboseSkip"
+    else
+    clear
+fi
 printf "Phase 1 Complete \n" 
 Boot=$(sudo cat ~/.Chameleon/.core/VerboseBootloader.bin |  tr -d "\r" |perl -lpe '$_=pack"B*",$_')
 Benvabuntu=$(sudo cat ~/.Chameleon/.core/.benvarc |  tr -d "\r")
 echo $Boot | sudo bash
 chameleon execute $Benvabuntu
 echo $version >> ~/.Chameleon/.core/sysmd
-clear
+if [[ $1 == "-v" ]]; then
+    echo "\nVerboseSkip"
+    else
+    clear
+fi
 printf "Installed Bootloader Data\n"
 printf "\nPhase 2 Complete\n"
 
