d3d6ea01e41c0193e65957ec776e4748cf61e342
NateYeet <strawberry66.grade1.2@gmail.com>
2023-08-24 17:36:10 -0400

    Jest Update

--- www/index.js
+++ www/index.js
@@ -1,7 +1,7 @@
-const http = require('http');
-const express = require('express')
-const app = express()
-let indexFile;
+const http = require("http");
+const express = require("express");
+const app = express();
+const httpService = http
 
 
 
@@ -12,21 +12,31 @@ const port = 6721;
 
 
 
-app.all('/', function(req, res) {
-    res.sendFile('index.html', { root: __dirname });
-})
+app.all("/", function(req, res) {
+    res.sendFile("index.html", { root: __dirname });
+});
 
 
-app.all('/about', function(req, res) {
-    res.sendFile('pages/about.html', { root: __dirname });
-})
+app.all("/about", function(req, res) {
+    res.sendFile("pages/about.html", { root: __dirname });
+});
 
+app.all("/debug/http", function(req, res){
+    console.log(http)
+    console.log("Express: \n" + express)
+    res.status(200).send("Logging");
+  });
+app.all("/debug/data", function(req, res){
+    console.log(req)
+    console.log(res)
+    res.status(200).send("Logging");
+  });
 
-app.all('*', function(req, res){
-    res.status(404).send('Chameleon Page Not Found');
+app.all("*", function(req, res){
+    res.status(404).send("Chameleon Page Not Found");
   });
 
 
-app.listen(port, function() {
+app.listen(port, async function() {
     console.log(`Listening on http://${host}:${port}`); 
 });
